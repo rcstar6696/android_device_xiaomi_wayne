d22967728e79d26919dfc8c38e7f5442281c77f6
Max Weffers <rcstar6696@gmail.com>
2020-05-23 15:03:29 +0200

    wayne: rootdir: import device specific init

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.device.rc
+
 # Properties
 include $(LOCAL_PATH)/vendor_prop.mk
 
